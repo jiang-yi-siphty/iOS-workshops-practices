0461016dc9369dca41299dae1b0525791aa537f9
Yi JIANG <jiang.yi@siphty.com>
2019-04-02 07:39:05 +1100

    no message

--- Session 9/S9t4  What's new in Swift 5.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Session 9/S9t4  What's new in Swift 5.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,3 +1,13 @@
-import UIKit
+let times = [
+    "first": 2,
+    "second": 43,
+    "three": 12,
+    "four": 3
+]
 
-var str = "Hello, playground"
+let compact = times.compactMap({ return $0.value > 10 ? $0 : nil })
+print(compact)
+
+let filter = times.filter { $0.value > 10
+}
+print(filter)
